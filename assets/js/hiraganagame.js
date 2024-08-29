8b5379c2363a27475952cfb3fc30bfde31dcfb74
Lauren Woods <laurenleighwoods@gmail.com>
2024-08-29 10:08:02 +0000

    Fix bugs, add dictionaries

--- assets/js/hiraganagame.js
+++ assets/js/hiraganagame.js
@@ -15,48 +15,30 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
+const vowels = [
     {
-        question:'What character is this? あ',
-        choice1:'I',
-        choice2:'O',
-        choice3:'A',
-        choice4:'E',
-        answer:3,
+        jp: "あ",
+        en: "A"
     },
     {
-        question:'What character is this? い',
-        choice1:'A',
-        choice2:'I',
-        choice3:'U',
-        choice4:'E',
-        answer:2,
+        jp: "い",
+        en: "I"
     },
     {
-        question:'What character is this? う',
-        choice1:'A',
-        choice2:'O',
-        choice3:'I',
-        choice4:'U',
-        answer:4,
+        jp: "う",
+        en: "U"
     },
     {
-        question:'What character is this? え',
-        choice1:'E',
-        choice2:'A',
-        choice3:'U',
-        choice4:'O',
-        answer:1,
+        jp: "え",
+        en: "E"
     },
     {
-        question:'What character is this? お',
-        choice1:'A',
-        choice2:'U',
-        choice3:'O',
-        choice4:'I',
-        answer:3,
-    },
+        jp: "お",
+        en: "O"
+    }
 ];
+
+let vowelsShuffled = vowels.sort(() => Math.random() - 0.5);
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
@@ -64,30 +46,28 @@ const MAX_QUESTIONS = 5;
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuesions = [...questions];
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        return window.location.assign('/hiraganaend.html');
+    if (vowelsShuffled.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign('hiraganaend.html');
     }
     questionCounter++;
     /* Progress Bar: https://www.youtube.com/watch?v=4bctmtuZVcM&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=7 */
     progressText.innerText = "Question " + questionCounter + "/" + MAX_QUESTIONS;
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+    vowelsShuffled.forEach((vowel, i) => {
+        if (i <= 3) {
+            question.innerText = `What character is this? ${vowelsShuffled[0].jp}`;
+            let randomOrder = Math.floor(Math.random() * 5);
+            choices[i].style.order = randomOrder;
+            choices[i].dataset["char"] = vowel.jp;
+            choices[i].innerText = vowel.en;
+        }
     });
 
-    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
@@ -97,25 +77,26 @@ choices.forEach(choice => {
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset["char"];
 
         /* Display Feedback: https://www.youtube.com/watch?v=_LYxkClHnV0&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=5 */
         const classToApply = 
-          selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+          selectedAnswer == vowelsShuffled[0].jp ? "correct" : "incorrect";
     
         if (classToApply == "correct") {
             incrementScore(CORRECT_BONUS);
         }
 
-        selectedChoice.parentElement.classList.add(classToApply);
+        selectedChoice.classList.add(classToApply);
         
         setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
+            vowelsShuffled.push(vowelsShuffled.splice(0, 1)[0]);
+            selectedChoice.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
     });
 });
-/* Score Increment: https://www.youtube.com/watch?v=BOQLbu_Crc0&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=6 */
+// /* Score Increment: https://www.youtube.com/watch?v=BOQLbu_Crc0&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=6 */
 incrementScore = num => {
     score += num;
     scoreText.innerText = score;
