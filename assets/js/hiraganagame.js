e586798aa17e8cb997f1634363630b16b251b4af
Lauren Woods <laurenleighwoods@gmail.com>
2024-08-31 12:22:45 +0000

    Add dictionary entries

--- assets/js/hiraganagame.js
+++ assets/js/hiraganagame.js
@@ -37,6 +37,298 @@ const vowels = [
         en: "O"
     }
 ];
+const kLetters= [
+    {
+        jp: "か",
+        en: "KA"
+    },
+    {
+        jp: "き",
+        en: "KI"
+    },
+    {
+        jp: "く",
+        en: "KU"
+    },
+    {
+        jp: "け",
+        en: "KE"
+    },
+    {
+        jp: "こ",
+        en: "KO"
+    }
+];
+const sLetters = [
+    {
+        jp: "さ",
+        en: "SA"
+    },
+    {
+        jp: "し",
+        en: "SHI"
+    },
+    {
+        jp: "す",
+        en: "SU"
+    },
+    {
+        jp: "せ",
+        en: "SE"
+    },
+    {
+        jp: "そ",
+        en: "SO"
+    }
+];
+const tLetters = [
+    {
+        jp: "た",
+        en: "TA"
+    },
+    {
+        jp: "ち",
+        en: "CHI"
+    },
+    {
+        jp: "つ",
+        en: "TSU"
+    },
+    {
+        jp: "て",
+        en: "TE"
+    },
+    {
+        jp: "と",
+        en: "TO"
+    }
+];
+const nLetters = [
+    {
+        jp: "な",
+        en: "NA"
+    },
+    {
+        jp: "に",
+        en: "NI"
+    },
+    {
+        jp: "ぬ",
+        en: "NU"
+    },
+    {
+        jp: "ね",
+        en: "NE"
+    },
+    {
+        jp: "の",
+        en: "NO"
+    }
+];
+const hLetters = [
+    {
+        jp: "は",
+        en: "HA"
+    },
+    {
+        jp: "ひ",
+        en: "HI"
+    },
+    {
+        jp: "ふ",
+        en: "FU"
+    },
+    {
+        jp: "へ",
+        en: "HE"
+    },
+    {
+        jp: "ほ",
+        en: "HO"
+    }
+];
+const mLetters = [
+    {
+        jp: "ま",
+        en: "MA"
+    },
+    {
+        jp: "み",
+        en: "MI"
+    },
+    {
+        jp: "む",
+        en: "MU"
+    },
+    {
+        jp: "め",
+        en: "ME"
+    },
+    {
+        jp: "も",
+        en: "MO"
+    }
+];
+const rLetters = [
+    {
+        jp: "ら",
+        en: "RA"
+    },
+    {
+        jp: "り",
+        en: "RI"
+    },
+    {
+        jp: "る",
+        en: "RU"
+    },
+    {
+        jp: "れ",
+        en: "RE"
+    },
+    {
+        jp: "ろ",
+        en: "RO"
+    }
+];
+const yLetters = [
+    {
+        jp: "や",
+        en: "YA"
+    },
+    {
+        jp: "ゆ",
+        en: "YU"
+    },
+    {
+        jp: "よ",
+        en: "YO"
+    }
+];
+const wLetters = [
+    {
+        jp: "わ",
+        en: "WA"
+    },
+    {
+        jp: "を",
+        en: "WO"
+    },
+    {
+        jp: "ん",
+        en: "N"
+    }
+];
+const gLetters= [
+    {
+        jp: "が",
+        en: "KA"
+    },
+    {
+        jp: "ぎ",
+        en: "KI"
+    },
+    {
+        jp: "ぐ",
+        en: "KU"
+    },
+    {
+        jp: "げ",
+        en: "KE"
+    },
+    {
+        jp: "ご",
+        en: "KO"
+    }
+];
+const zLetters = [
+    {
+        jp: "ざ",
+        en: "ZA"
+    },
+    {
+        jp: "じ",
+        en: "JI"
+    },
+    {
+        jp: "ず",
+        en: "ZU"
+    },
+    {
+        jp: "ぜ",
+        en: "ZE"
+    },
+    {
+        jp: "ぞ",
+        en: "ZO"
+    }
+];
+const dLetters = [
+    {
+        jp: "だ",
+        en: "DA"
+    },
+    {
+        jp: "ぢ",
+        en: "DJI"
+    },
+    {
+        jp: "づ",
+        en: "DZU"
+    },
+    {
+        jp: "で",
+        en: "DE"
+    },
+    {
+        jp: "ど",
+        en: "DO"
+    }
+];
+const bLetters = [
+    {
+        jp: "ば",
+        en: "BA"
+    },
+    {
+        jp: "び",
+        en: "BI"
+    },
+    {
+        jp: "ぶ",
+        en: "BU"
+    },
+    {
+        jp: "べ",
+        en: "BE"
+    },
+    {
+        jp: "ぼ",
+        en: "BO"
+    }
+];
+const pLetters = [
+    {
+        jp: "ぱ",
+        en: "PA"
+    },
+    {
+        jp: "ぴ",
+        en: "PI"
+    },
+    {
+        jp: "ぷ",
+        en: "PU"
+    },
+    {
+        jp: "ぺ",
+        en: "PE"
+    },
+    {
+        jp: "ぽ",
+        en: "PO"
+    }
+];
 
 let vowelsShuffled = vowels.sort(() => Math.random() - 0.5);
 
