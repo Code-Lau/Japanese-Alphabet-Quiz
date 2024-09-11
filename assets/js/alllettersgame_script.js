dd6d97abed676c159a2d68c4bc9b5958534798a2
Lauren Woods <laurenleighwoods@gmail.com>
2024-09-11 16:57:06 +0000

    Rename switch cases

--- assets/js/alllettersgame_script.js
+++ assets/js/alllettersgame_script.js
@@ -4,18 +4,18 @@
 
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById("progressText")
-const scoreText = document.getElementById("score")
-const progressBarFull = document.getElementById("progressBarFull")
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuestions = [];
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 5;
+let MAX_QUESTIONS = 5;
 
 const urlParams = new URLSearchParams(window.location.search);
 const quizParam = urlParams.get("quiz");
@@ -23,8 +23,14 @@ const quizParam = urlParams.get("quiz");
 let selectedQuiz;
 
 switch(quizParam) {
-    case "allLettersHK":
+    case "all":
         selectedQuiz = allLettersHKShuffled;
+        break;
+    case "s":
+        selectedQuiz = allSeionShuffled;
+        break;
+    case "d":
+        selectedQuiz = allDakuonShuffled;
         break;
     default:
         selectedQuiz = allLettersHKShuffled;
@@ -40,21 +46,27 @@ startGame = () => {
 
 getNewQuestion = () => {
     if (selectedQuiz.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        return window.location.assign('quizend.html.html');
+        return window.location.assign('quizend.html');
     }
-    questionCounter++;
     /* Progress Bar: https://www.youtube.com/watch?v=4bctmtuZVcM&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=7 */
-    progressText.innerText = "Question " + questionCounter + "/" + MAX_QUESTIONS;
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    selectedQuiz.forEach((letter, i) => {
-        if (i <= 3) {
-            question.innerText = `What character is this? ${selectedQuiz[0].jp}`;
-            let randomOrder = Math.floor(Math.random() * 5);
-            choices[i].style.order = randomOrder;
-            choices[i].dataset["char"] = letter.jp;
-            choices[i].innerText = letter.en;
-        }
+    const currentLetter = selectedQuiz[0];
+
+    let answerPool = [...selectedQuiz].sort(() => Math.random() - 0.5).slice(0, 4);
+
+    if (!answerPool.includes(currentLetter)) {
+        answerPool[Math.floor(Math.random() * 4)] = currentLetter;
+    }
+
+    question.innerText = `What character is this? ${currentLetter.jp}`;
+    
+    choices.forEach((choice, i) => {
+        const letter = answerPool[i];
+        choice.dataset["char"] = letter.jp;
+        choice.innerText = letter.en;
     });
 
     acceptingAnswers = true;
@@ -69,26 +81,26 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset["char"];
 
         /* Display Feedback: https://www.youtube.com/watch?v=_LYxkClHnV0&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=5 */
-        const classToApply = 
-          selectedAnswer == selectedQuiz[0].jp ? "correct" : "incorrect";
+        const classToApply = selectedAnswer == selectedQuiz[0].jp ? "correct" : "incorrect";
     
-        if (classToApply == "correct") {
+        if (classToApply === "correct") {
             incrementScore(CORRECT_BONUS);
         }
 
         selectedChoice.classList.add(classToApply);
         
-        setTimeout( () => {
+        setTimeout(() => {
             selectedQuiz.push(selectedQuiz.splice(0, 1)[0]);
             selectedChoice.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
     });
 });
+
 // /* Score Increment: https://www.youtube.com/watch?v=BOQLbu_Crc0&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx&index=6 */
 incrementScore = num => {
     score += num;
     scoreText.innerText = score;
-}
+};
 
 startGame();
